d51c1f18b700ff1d55a5555dc9be98815a9ac153
Carlos Feria <carlosthe19916@gmail.com>
2018-11-06 15:58:43 -0500

    fix(docs): update docs

--- docs/DocumentResponseRepresentation.md
+++ docs/DocumentResponseRepresentation.md
@@ -4,28 +4,53 @@
 ## Properties
 Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
-**id** | **String** |  |  [optional]
-**documentId** | **String** |  |  [optional]
-**documentType** | **String** |  |  [optional]
-**createdTimestamp** | [**OffsetDateTime**](OffsetDateTime.md) |  |  [optional]
-**issueDate** | [**OffsetDateTime**](OffsetDateTime.md) |  |  [optional]
-**documentCurrencyCode** | **String** |  |  [optional]
-**customerRegistrationName** | **String** |  |  [optional]
-**customerAssignedAccountId** | **String** |  |  [optional]
-**customerElectronicMail** | **String** |  |  [optional]
-**status** | **String** |  |  [optional]
-**statusCode** | **Integer** |  |  [optional]
-**statusMessage** | **String** |  |  [optional]
-**bajaEnProceso** | **Boolean** |  |  [optional]
-**closed** | **Boolean** |  |  [optional]
-**summaryVoided** | **Boolean** |  |  [optional]
-**attributes** | [**Map&lt;String, List&lt;String&gt;&gt;**](List.md) |  |  [optional]
-**requiredActions** | **List&lt;String&gt;** |  |  [optional]
-**attachedDocumentsAsOrigin** | [**List&lt;DocumentResponseRepresentation&gt;**](DocumentResponseRepresentation.md) |  |  [optional]
-**attachedDocumentsAsDestiny** | [**List&lt;DocumentResponseRepresentation&gt;**](DocumentResponseRepresentation.md) |  |  [optional]
-**documentLines** | [**List&lt;Map&lt;String, String&gt;&gt;**](Map.md) |  |  [optional]
+**id** | **String** | ID único del comprobante |  [optional]
+**documentId** | **String** | Serie y número del comprobante |  [optional]
+**documentType** | [**DocumentTypeEnum**](#DocumentTypeEnum) | Tipo de comprobante |  [optional]
+**createdTimestamp** | [**OffsetDateTime**](OffsetDateTime.md) | Fecha en el que el comprobante se envió a OPENFACT |  [optional]
+**issueDate** | [**OffsetDateTime**](OffsetDateTime.md) | Fecha en el que el comprobante se creó. Esta fecha será utilizada para declarar a la SUNAT |  [optional]
+**documentCurrencyCode** | **String** | Moneda del comprobante |  [optional]
+**customerRegistrationName** | **String** | Nombre del cliente |  [optional]
+**customerAssignedAccountId** | **String** | Número de documento del cliente |  [optional]
+**customerElectronicMail** | **String** | Correo electrónico del cliente |  [optional]
+**status** | [**StatusEnum**](#StatusEnum) | Estado del comprobante en la SUNAT |  [optional]
+**statusCode** | **Integer** | Código del estado del comprobante en la SUNAT. Los valores son números entre 0 y 5000 |  [optional]
+**statusMessage** | **String** | Mensaje del estado del comprobante en la SUNAT. |  [optional]
+**summaryVoided** | **Boolean** | Válido solo para boletas. Determina si una boleta fue marcada para ser dada de baja o no |  [optional]
+**bajaEnProceso** | **Boolean** | Válido solo para boletas. Determina si una boleta fue marcada para ser dada de baja o no |  [optional]
+**closed** | **Boolean** | Determina si el comprobante se puede o no editar. Si FALSE entonces se puede editar y su firma electrónica todavia no es válida |  [optional]
+**attributes** | [**Map&lt;String, List&lt;String&gt;&gt;**](List.md) | Conjunto de informacion extra que el comprobante guarda. Esta información varía dependiendo del documento |  [optional]
+**requiredActions** | **List&lt;String&gt;** | Conjunto de acciones pendientes en el comprobante. |  [optional]
+**attachedDocumentsAsOrigin** | [**List&lt;DocumentResponseRepresentation&gt;**](DocumentResponseRepresentation.md) | Lista de comprobantes afectados por el presente comprobante |  [optional]
+**attachedDocumentsAsDestiny** | [**List&lt;DocumentResponseRepresentation&gt;**](DocumentResponseRepresentation.md) | Lista de comprobantes que afectan al presente comprobante |  [optional]
+**documentLines** | [**List&lt;Map&lt;String, String&gt;&gt;**](Map.md) | Lista de productos o servicios contenidos dentro del comprobante de pago |  [optional]
+**xmlDigestValue** | **String** | HASH de la firma digital del comprobante electrónico |  [optional]
 **xmlBase64** | **String** |  |  [optional]
 **pdfBase64** | **String** |  |  [optional]
+
+
+<a name="DocumentTypeEnum"></a>
+## Enum: DocumentTypeEnum
+Name | Value
+---- | -----
+INVOICE | &quot;INVOICE&quot;
+CREDIT_NOTE | &quot;CREDIT_NOTE&quot;
+DEBIT_NOTE | &quot;DEBIT_NOTE&quot;
+PERCEPTION | &quot;PERCEPTION&quot;
+RETENTION | &quot;RETENTION&quot;
+VOIDED_DOCUMENTS | &quot;VOIDED_DOCUMENTS&quot;
+SUMMARY_DOCUMENTS | &quot;SUMMARY_DOCUMENTS&quot;
+
+
+<a name="StatusEnum"></a>
+## Enum: StatusEnum
+Name | Value
+---- | -----
+ACEPTADO | &quot;ACEPTADO&quot;
+RECHAZADO | &quot;RECHAZADO&quot;
+BAJA | &quot;BAJA&quot;
+EXCEPCION | &quot;EXCEPCION&quot;
+EN_PROCESO | &quot;EN_PROCESO&quot;
 
 
 
--- src/main/java/org/openfact/rest/idm/DocumentResponseRepresentation.java
+++ src/main/java/org/openfact/rest/idm/DocumentResponseRepresentation.java
@@ -1,38 +1,133 @@
 package org.openfact.rest.idm;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+@ApiModel(subTypes = {Invoice.class, CreditNote.class, DebitNote.class})
 public class DocumentResponseRepresentation {
 
+    @ApiModelProperty(
+            value = "ID único del comprobante",
+            example = "ab84523c-85d9-4545-b679-ba9522a1ed1f"
+    )
     private String id;
+
+    @ApiModelProperty(
+            value = "Serie y número del comprobante",
+            example = "F001-1"
+    )
     private String documentId;
+
+    @ApiModelProperty(
+            value = "Tipo de comprobante",
+            allowableValues = "INVOICE, CREDIT_NOTE, DEBIT_NOTE, PERCEPTION, RETENTION, VOIDED_DOCUMENTS, SUMMARY_DOCUMENTS",
+            example = "INVOICE"
+    )
     private String documentType;
+
+    @ApiModelProperty(
+            value = "Fecha en el que el comprobante se envió a OPENFACT",
+            example = "2018-11-06T13:12:12.924+0000"
+    )
     private Date createdTimestamp;
+
+    @ApiModelProperty(
+            value = "Fecha en el que el comprobante se creó. Esta fecha será utilizada para declarar a la SUNAT",
+            example = "2018-11-06T05:00:00.000+0000"
+    )
     private Date issueDate;
 
+    @ApiModelProperty(
+            value = "Moneda del comprobante"
+    )
     private String documentCurrencyCode;
+
+    @ApiModelProperty(
+            value = "Nombre del cliente"
+    )
     private String customerRegistrationName;
+
+    @ApiModelProperty(
+            value = "Número de documento del cliente"
+    )
     private String customerAssignedAccountId;
+
+    @ApiModelProperty(
+            value = "Correo electrónico del cliente"
+    )
     private String customerElectronicMail;
 
+    @ApiModelProperty(
+            value = "Estado del comprobante en la SUNAT",
+            allowableValues = "ACEPTADO, RECHAZADO, BAJA, EXCEPCION, EN_PROCESO",
+            example = "ACEPTADO"
+    )
     private String status;
+
+    @ApiModelProperty(
+            value = "Código del estado del comprobante en la SUNAT. Los valores son números entre 0 y 5000",
+            example = "0"
+    )
     private Integer statusCode;
+
+    @ApiModelProperty(
+            value = "Mensaje del estado del comprobante en la SUNAT.",
+            example = "Comprobante fue aceptado"
+    )
     private String statusMessage;
 
-    private boolean bajaEnProceso;
-    private boolean closed;
+    @ApiModelProperty(
+            value = "Válido solo para boletas. Determina si una boleta fue marcada para ser dada de baja o no"
+    )
     private boolean summaryVoided;
 
+    @ApiModelProperty(
+            value = "Válido solo para boletas. Determina si una boleta fue marcada para ser dada de baja o no"
+    )
+    private boolean bajaEnProceso;
+
+    @ApiModelProperty(
+            value = "Determina si el comprobante se puede o no editar. Si FALSE entonces se puede editar y su firma electrónica todavia no es válida",
+            example = "Comprobante fue aceptado"
+    )
+    private boolean closed;
+
+    @ApiModelProperty(
+            value = "Conjunto de informacion extra que el comprobante guarda. Esta información varía dependiendo del documento"
+    )
     private Map<String, List<String>> attributes;
+
+    @ApiModelProperty(
+            value = "Conjunto de acciones pendientes en el comprobante.",
+            allowableValues = "SEND_TO_THIRD_PARTY, SEND_TO_CUSTOMER, CONSULTAR_TICKET",
+            example = "SEND_TO_THIRD_PARTY"
+    )
     private Set<String> requiredActions;
 
+    @ApiModelProperty(
+            value = "Lista de comprobantes afectados por el presente comprobante"
+    )
     private List<DocumentResponseRepresentation> attachedDocumentsAsOrigin;
+
+    @ApiModelProperty(
+            value = "Lista de comprobantes que afectan al presente comprobante"
+    )
     private List<DocumentResponseRepresentation> attachedDocumentsAsDestiny;
 
+    @ApiModelProperty(
+            value = "Lista de productos o servicios contenidos dentro del comprobante de pago"
+    )
     private List<Map<String, String>> documentLines;
+
+    @ApiModelProperty(
+            value = "HASH de la firma digital del comprobante electrónico"
+    )
+    private String xmlDigestValue;
 
     private String xmlBase64;
     private String pdfBase64;
@@ -213,4 +308,11 @@ public class DocumentResponseRepresentation {
         this.bajaEnProceso = bajaEnProceso;
     }
 
+    public String getXmlDigestValue() {
+        return xmlDigestValue;
+    }
+
+    public void setXmlDigestValue(String xmlDigestValue) {
+        this.xmlDigestValue = xmlDigestValue;
+    }
 }
